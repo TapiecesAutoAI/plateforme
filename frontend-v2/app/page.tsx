11ab794f64407be489de4ec12c99c39069f207b1
Zeki Turkkan <306221773+TapiecesAutoAI@users.noreply.github.com>
2026-08-20 13:44:15 +0200

    Set showroom as application homepage

--- frontend-v2/app/page.tsx
+++ frontend-v2/app/page.tsx
@@ -1,9 +1,5 @@
-﻿import {
-  redirect,
-} from "next/navigation";
+import { redirect } from "next/navigation";
 
-export default function Home() {
-  redirect(
-    "/diagnostic-v2",
-  );
+export default function HomePage() {
+  redirect("/showroom");
 }
